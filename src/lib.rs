f71a211932ef167ea096bee98b4c9147a6c4d82e
caojen <caojen@mail2.sysu.edu.cn>
2023-04-27 22:53:37 +0800

    feat: update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,52 @@
-extern crate core;
+//! Google-Oauth
+//!
+//! A server-side google oauth2 verification library for rust.
+//!
+//! # Installation
+//! This library is hosted on [crates.io](https://crates.io/crates/mysql_async/)
+//!
+//! ```toml
+//! [dependencies]
+//! google-oauth = "1"
+//! ```
+//!
+//! # Usage
+//! The Library provides API to verify `id_token` from Google.
+//!
+//! ## Example: Blocking
+//! ```rust
+//! // With blocking client, we don't need async runtime.
+//! use google_oauth::Client; // This is a blocking client
+//!
+//! let client_id = "The client_id generated from google...";
+//! let client = Client::new(client_id); // now we get this.
+//!
+//! let data = client.validate_id_token("The id_token you want to validate").unwrap();
+//!
+//! // if ok, we get the data...
+//! let sub = data.sub.as_str();
+//! println!("Hello, user (sub = {}) now is online", sub);
+//! ```
+//!
+//! ## Example: Async
+//! ```rust
+//! // When you use async client, remember to add a async runtime (e.g, `tokio` or `async_std`)
+//! use google_oauth::AsyncClient; // This is a async client
+//!
+//! // It works just like the blocking client, excepts the verification step.
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let client_id = "...";
+//!     let client = AsyncClient::new(client_id);
+//!
+//!     /// note: use `await`
+//!     let data = client.validate_id_token("The id_token you want to validate").await.unwrap();
+//!
+//!     // it `unwrap()` is ok, we get the data ...
+//!     println!("Hello, user (sub = {}) now is online", sub);
+//! }
+//! ```
 
 mod client;
 mod async_client;
