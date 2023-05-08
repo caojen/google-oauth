eb81ad0c3cfe45efa883ba9c857ce41bf6f21c24
caojen <43436876+caojen@users.noreply.github.com>
2023-05-08 15:05:17 +0800

    feat: make optional fields with Option<T> (#2)
    * feat: modify output and make optional fields as  Option<T>
    
    * feat: set version to 1.1.0
    
    * lint: fix

--- src/output.rs
+++ src/output.rs
@@ -1,21 +1,31 @@
 use serde::{Serialize, Deserialize};
 
 /// `GooglePayload` is the user data from google.
+/// see https://developers.google.com/identity/openid-connect/openid-connect for more info.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[non_exhaustive]
 pub struct GooglePayload {
-    pub iss: String,
-    pub nbf: u64,
+    // These fields are marked `always`.
     pub aud: String,
-    pub sub: String,
-    pub email: String,
-    pub email_verified: bool,
-    pub azp: String,
-    pub name: String,
-    pub picture: String,
-    pub given_name: String,
-    pub family_name: String,
-    pub iat: u64,
     pub exp: u64,
-    pub jti: String,
+    pub iat: u64,
+    pub iss: String,
+    pub sub: String,
+
+    // These fields are optional.
+    pub at_hash: Option<String>,
+    pub azp: Option<String>,
+    pub email: Option<String>,
+    pub email_verified: Option<bool>,
+    pub family_name: Option<String>,
+    pub given_name: Option<String>,
+    pub hd: Option<String>,
+    pub locale: Option<String>,
+    pub name: Option<String>,
+    pub nonce: Option<String>,
+    pub picture: Option<String>,
+
+    // These fields doesn't list in document, but it exists
+    pub nbf: Option<u64>,
+    pub jti: Option<String>,
 }
