415b72b23183602a72535a35016dd686219fc1d8
caojen <caojen@mail2.sysu.edu.cn>
2024-04-20 04:12:15 +0800

    lint: remove unused import

--- src/async_client.rs
+++ src/async_client.rs
@@ -6,7 +6,7 @@ use std::time::{Duration, Instant};
 use lazy_static::lazy_static;
 use log::debug;
 use async_rwlock::RwLock;
-use crate::{DEFAULT_TIMEOUT, Error, GOOGLE_OAUTH_V3_USER_INFO_API, GOOGLE_SA_CERTS_URL, GoogleAccessTokenPayload, GooglePayload, IDTokenClientIDNotFoundError, MyResult, utils};
+use crate::{DEFAULT_TIMEOUT, GOOGLE_OAUTH_V3_USER_INFO_API, GOOGLE_SA_CERTS_URL, GoogleAccessTokenPayload, GooglePayload, MyResult, utils};
 use crate::certs::{Cert, Certs};
 use crate::jwt_parser::JwtParser;
 use crate::validate::id_token;
